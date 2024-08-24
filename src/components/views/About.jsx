17b14c097ac76cdb3a9eca9e1264c8178a3ab49f
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-08-24 11:42:53 +0530

    s

--- src/components/views/About.jsx
+++ src/components/views/About.jsx
@@ -216,7 +216,7 @@ function About() {
           </div>
           <div class="col-md-5 About-AvatarSubDiv">
             <div className="manAvatar ">
-              <img src="/background image netflix-5feb1047.jpg" alt="Avatar-manSeba" />
+              <img className="manAvatar" src='https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimg.freepik.com%2Ffoto-gratis%2Fexperiencia-programacion-persona-que-trabaja-codigos-computadora_23-2150010125.jpg%3Fsize%3D626%26ext%3Djpg%26ga%3DGA1.1.2008272138.1724371200%26semt%3Dais_hybrid&sp=1724479645Tfa2491eee21f6b795bbdebc5076b690368e121f93317e5730d9be0e1581ffffb' alt="Avatar-manSeba" />
             </div>
           </div>
         </div>

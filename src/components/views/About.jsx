5aa73943a98383ff29c83128a1fc83220af39e00
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-08-24 12:54:12 +0530

    slider

--- src/components/views/About.jsx
+++ src/components/views/About.jsx
@@ -12,9 +12,24 @@ import {
 } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiMysql, SiRedux, SiInsomnia } from "react-icons/si";
-import avatarSrc from '/Avatar-man.png';
+import avatarSrc from "/Avatar-man.png";
+import { Carousel } from "react-bootstrap";
 
 function About() {
+  const techIcons = [
+    { icon: FaHtml5, name: "HTML" },
+    { icon: FaCss3, name: "CSS" },
+    { icon: IoLogoJavascript, name: "JavaScript" },
+    { icon: FaReact, name: "React" },
+    { icon: FaNodeJs, name: "NodeJS" },
+    { icon: SiRedux, name: "Redux" },
+    { icon: FaBootstrap, name: "Bootstrap" },
+    { icon: FaGitAlt, name: "Git" },
+    { icon: FaGithub, name: "Github" },
+    { icon: SiInsomnia, name: "Insomnia" },
+    { icon: FaFigma, name: "Figma" }
+  ];
+
   return (
     <>
       <nav className="p-3 position-relative" style={{ zIndex: 2 }}>
@@ -151,67 +166,10 @@ function About() {
                 .
               </p>
             </div>
-            <div className="snap-section d-flex justify-content-center align-items-center about-lg-padding pb-5">
-              <div className="techAndTools mx-auto">
-                <div className=" my-3">
-                  <h2 className="fw-bold mb-3 bold-text ">
-                    TECHNOLOGIES & TOOLS
-                  </h2>
-                  <p>
-                    This website was built using these technologies and tools:
-                  </p>
-                </div>
-                <div className="row d-flex justify-content-around align-content-around glass ">
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaHtml5 className="icons html" size="2rem" />
-                    <h5 className="mt-2">HTML</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaCss3 className="icons css" size="2rem" />
-                    <h5 className="mt-2">CSS</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <IoLogoJavascript className="icons js" size="2rem" />
-                    <h5 className="mt-2">JavaScript</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaReact className="icons react" size="2rem" />
-                    <h5 className="mt-2">React</h5>
-                  </div>
-                  {/* <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <SiMysql className="icons mysql" size="2rem" />
-                    <h5 className="mt-2">MySQL</h5>
-                  </div> */}
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaNodeJs className="icons nodejs" size="2rem" />
-                    <h5 className="mt-2">NodeJS</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <SiRedux className="icons redux" size="2rem" />
-                    <h5 className="mt-2">Redux</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaBootstrap className="icons bootstrap" size="2rem" />
-                    <h5 className="mt-2">Bootstrap</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaGitAlt className="icons git" size="2rem" />
-                    <h5 className="mt-2">Git</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaGithub className="icons github" size="2rem" />
-                    <h5 className="mt-2">Github</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <SiInsomnia className="icons insomnia" size="2rem" />
-                    <h5 className="mt-2">Insomnia</h5>
-                  </div>
-                  <div className="col-4 col-md-3 mt-3 my-2 d-flex flex-column align-items-center text-center div-icon">
-                    <FaFigma className="icons figma" size="2rem" />
-                    <h5 className="mt-2">Figma</h5>
-                  </div>
-                </div>
-              </div>
+
+            <div>
+              {/* <h2 className="fw-bold mb-3 bold-text ">TECHNOLOGIES & TOOLS</h2>
+              <p>This website was built using these technologies and tools:</p> */}
             </div>
           </div>
           <div class="col-md-5 About-AvatarSubDiv">
@@ -219,19 +177,20 @@ function About() {
               <img className="manAvatar" src='https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimg.freepik.com%2Ffoto-gratis%2Fexperiencia-programacion-persona-que-trabaja-codigos-computadora_23-2150010125.jpg%3Fsize%3D626%26ext%3Djpg%26ga%3DGA1.1.2008272138.1724371200%26semt%3Dais_hybrid&sp=1724479645Tfa2491eee21f6b795bbdebc5076b690368e121f93317e5730d9be0e1581ffffb' alt="Avatar-manSeba" />
             </div>
           </div>
+          <div className="tech-slider-container mt-4">
+            <div className="tech-slider">
+              {[...techIcons, ...techIcons].map((tech, index) => (
+                <div className="tech-slide" key={index}>
+                  <tech.icon className={`icons ${tech.name.toLowerCase()}`} size="2rem" color="white" />
+                  <h5 className="mt-2 text-white">{tech.name}</h5>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
       <div className="AboutSubDiv2 mb-5">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? " text textColor fs-5 btn me-2"
-              : "inactiveNavbarButton  btn me-2"
-          }
-        >
-          <span className="hover homeButton"> Go to Home </span>
-        </NavLink>
+        
       </div>
     </>
   );

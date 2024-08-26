c77610ce2249366b7f05a8eada7841f376cec972
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-08-26 09:49:50 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import NavBar from "./Navbar";
-import Presentation from "./views/Presentation";
+
 
 function Layout() {
   return (
     <div className="">
-      {/* <NavBar /> */}
       <Outlet />
     </div>
   );
